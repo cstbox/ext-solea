fb16449607ea66103a1fc786a83370d54d3fe3b7
Eric Pascual <eric.pascual@cstb.fr>
2017-05-11 14:21:17 +0200

    upgraded to modbus extension version 2.x

--- lib/python/pycstbox/hal/drivers/solea.py
+++ lib/python/pycstbox/hal/drivers/solea.py
@@ -25,7 +25,9 @@ import pycstbox.solea.aj12b as aj12b
 import pycstbox.solea.aj42 as aj42
 import pycstbox.solea.ad12b as ad12b
 import pycstbox.solea.scs as scs
+
 from pycstbox.hal import hal_device
+from pycstbox.modbus import RTUModbusHALDevice
 
 _logger = logging.getLogger('solea')
 
@@ -33,7 +35,7 @@ DEFAULT_PRECISION = 3
 
 
 @hal_device(device_type="solea.ad12b", coordinator_type="modbus")
-class AD12B(haldev.PolledDevice):
+class AD12B(RTUModbusHALDevice):
     """ HAL device modeling the Solea AD12B module.
 
     DC multi-parameters module.
@@ -41,15 +43,15 @@ class AD12B(haldev.PolledDevice):
     The extension adds the support of polling requests and CSTBox events
     publishing on D-Bus.
     """
-    def __init__(self, coord, cfg):
-        super(AD12B, self).__init__(coord, cfg)
+    def __init__(self, coord_cfg, dev_cfg):
+        super(AD12B, self).__init__(coord_cfg, dev_cfg)
         self._hwdev = ad12b.AD12BInstrument(
-            coord.port, cfg.address, cfg.U_range, cfg.I_range
+            coord_cfg.port, dev_cfg.address, dev_cfg.U_range, dev_cfg.I_range
         )
 
 
 @hal_device(device_type="solea.aj12b", coordinator_type="modbus")
-class AJ12B(haldev.PolledDevice):
+class AJ12B(RTUModbusHALDevice):
     """ HAL device modeling the Solea AJ12B module.
 
     Single phase AC multi-parameters module.
@@ -57,15 +59,15 @@ class AJ12B(haldev.PolledDevice):
     The extension adds the support of polling requests and CSTBox events
     publishing on D-Bus.
     """
-    def __init__(self, coord, cfg):
-        super(AJ12B, self).__init__(coord, cfg)
+    def __init__(self, coord_cfg, dev_cfg):
+        super(AJ12B, self).__init__(coord_cfg, dev_cfg)
         self._hwdev = aj12b.AJ12BInstrument(
-            coord.port, cfg.address, cfg.U_range, cfg.I_range
+            coord_cfg.port, dev_cfg.address, dev_cfg.U_range, dev_cfg.I_range
         )
 
 
 @hal_device(device_type="solea.aj42b", coordinator_type="modbus")
-class AJ42B(haldev.PolledDevice):
+class AJ42B(RTUModbusHALDevice):
     """ HAL device modeling the Solea AJ42B module.
 
     Three phases AC multi-parameters module.
@@ -73,20 +75,20 @@ class AJ42B(haldev.PolledDevice):
     The extension adds the support of polling requests and CSTBox events
     publishing on D-Bus.
     """
-    def __init__(self, coord, cfg):
-        super(AJ42B, self).__init__(coord, cfg)
+    def __init__(self, coord_cfg, dev_cfg):
+        super(AJ42B, self).__init__(coord_cfg, dev_cfg)
         self._hwdev = aj42.AJ42BInstrument(
-            coord.port, cfg.address, cfg.U_range, cfg.I_range
+            coord_cfg.port, dev_cfg.address, dev_cfg.U_range, dev_cfg.I_range
         )
 
 
 @hal_device(device_type="solea.scs", coordinator_type="modbus")
-class SCS(haldev.PolledDevice):
+class SCS(RTUModbusHALDevice):
     """ HAL device modeling the Solea SCS photo-voltaic module.
 
     The extension adds the support of polling requests and CSTBox events
     publishing on D-Bus.
     """
-    def __init__(self, coord, cfg):
-        super(SCS, self).__init__(coord, cfg)
-        self._hwdev = scs.SCSInstrument(coord.port, cfg.address)
+    def __init__(self, coord_cfg, dev_cfg):
+        super(SCS, self).__init__(coord_cfg, dev_cfg)
+        self._hwdev = scs.SCSInstrument(coord_cfg.port, dev_cfg.address)
